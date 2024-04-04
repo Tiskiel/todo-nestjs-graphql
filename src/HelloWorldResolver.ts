aabc21958ca1b495e914f7c81e085172aa9b4e20
Wanty Jean-François <wanty.jf@gmail.com>
2024-04-04 21:04:32 +0200

    Fix with the appropriate decorators

--- src/HelloWorldResolver.ts
+++ src/HelloWorldResolver.ts
@@ -1,8 +1,9 @@
-import { Resolver } from '@nestjs/graphql';
+import { Query, Resolver } from '@nestjs/graphql';
 import { HelloWorld } from './HelloWorld';
 
 @Resolver(HelloWorld)
 export class HelloWorldResolver {
+  @Query(() => HelloWorld)
   async helloWorld(): Promise<HelloWorld> {
     return {
       message: 'Hello, World!',
