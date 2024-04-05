a3d53b3eeb5860f36108c4d35b303a255278c550
Wanty Jean-François <wanty.jf@gmail.com>
2024-04-05 09:40:34 +0200

    Add PrismaService

--- src/Core/CoreModule.ts
+++ src/Core/CoreModule.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import GraphQLModule from './GraphQLModule';
+import { PrismaService } from './Prisma/PrismaService';
 
 @Module({
   imports: [GraphQLModule],
   controllers: [],
-  providers: [],
+  providers: [PrismaService],
 })
 export class CoreModule {}
