a2f48c6882521164bf8d027cf0f99b13eef98232
Wanty Jean-François <wanty.jf@gmail.com>
2024-04-08 10:24:46 +0200

    Add createUser into UserResolver

--- src/API/User/Resolvers/UserResolver.ts
+++ src/API/User/Resolvers/UserResolver.ts
@@ -1,13 +1,17 @@
 import { UserFindAllUseCase } from '../UseCases/UserFindAllUseCase';
-import { Args, Int, Query, Resolver } from '@nestjs/graphql';
+import { Args, Int, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { User } from '../Entities/User';
 import { UserFindByIdUseCase } from '../UseCases/UserFindByIdUseCase';
+import { CreateUserDto } from '../Dto/CreateUserDto';
+import { UserType } from '../Type/UserType';
+import { UserCreateUseCase } from '../UseCases/UserCreateUseCase';
 
 @Resolver(() => User)
 export class UserResolver {
   constructor(
     private readonly _findAll: UserFindAllUseCase,
     private readonly _findById: UserFindByIdUseCase,
+    private readonly _create: UserCreateUseCase,
   ) {}
 
   @Query(() => [User])
@@ -20,5 +24,10 @@ export class UserResolver {
     @Args('id', { type: () => Int }) id: number,
   ): Promise<User> {
     return await this._findById.handle(id);
+  }
+
+  @Mutation(() => User)
+  async createUser(@Args('data') data: CreateUserDto): Promise<UserType> {
+    return await this._create.handle(data);
   }
 }
